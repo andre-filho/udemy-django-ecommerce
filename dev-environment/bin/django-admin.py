ff3bf580fa92929308f7c67ea78a7864b77315b0
Andre Filho <andre.filho001@outlook.com>
2018-02-06 09:36:34 -0200

    [ADD] virtual environment for development [ADD] ecommerce project folder

---
+++ dev-environment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andre/Documents/programacao_pessoal/udemy/django-ecommerce/dev-environment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
