56deaa364bf304455a6c64161d34524e18712c0a
Andre Filho <andre.filho001@outlook.com>
2018-02-15 11:16:36 -0200

    [UPDATE] lecture 22
    product model

--- dev-environment/src/products/models.py
+++ dev-environment/src/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):    # CamelCase modafocka
+    title       = models.CharField(max_length=120)
+    description = models.TextField()
+    price       = models.DecimalField(
+        decimal_places = 2,
+        max_digits = 20,
+        default = 0.99
+    )
+    
