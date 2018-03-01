a0f539f30fb4e459fd57d7cbdaf57d52eca7ea35
Andre Filho <andre.filho001@outlook.com>
2018-03-01 16:40:36 -0300

    [UPDATE] lecture 26 [COMMITAO] :shit:
    image and file field with all the stuff that I will need

--- dev-environment/src/products/models.py
+++ dev-environment/src/products/models.py
@@ -1,4 +1,23 @@
+import random
+import os
+
 from django.db import models
+
+def get_filename_ext(filename):
+    base_name = os.path.basename(filename)
+    name, ext = os.path.splitext(filename)
+    return name, ext
+
+# large files upload guide: http://kirr.com/e1133t
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1, 91209319320)
+    name, ext =  get_filename_ext(filename)
+    final_name = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    # py3.6
+    # final_name = f'{new_filename}{ext}'
+    return "products/{r}/{name}".format(r=new_filename, name=final_name)
 
 # Create your models here.
 class Product(models.Model):    # CamelCase modafocka
@@ -8,6 +27,11 @@ class Product(models.Model):    # CamelCase modafocka
         decimal_places = 2,
         max_digits = 20,
         default = 0.99
+    )
+    image       = models.ImageField(    # pip install pillow
+        upload_to = upload_image_path,
+        null = True,
+        blank = True
     )
 
     def __str__(self):      # python3
